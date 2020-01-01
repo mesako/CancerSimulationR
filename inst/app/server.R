7ccab316f55b9c4fe3ebf090ebc56e15bfe82084
mesako <mesako@stanford.edu>
2020-01-01 15:16:45 -0800

    update simulation set-up tab

--- inst/app/server.R
+++ inst/app/server.R
@@ -4,15 +4,49 @@ library(ggplot2)
 library(reshape)
 
 server <- function(input, output) {
-  
-  react.vals <- reactiveValues()
+  ### SIMULATION SET-UP
+  text_values <- reactiveValues()
+  observeEvent(input$button1, {
+    text_values$mutrate <- input$mutationrate
+    text_values$maxdiv <- input$maxdivisions
+    text_values$rounds <- input$numrounds
+    map_dim <- c(10, 10)
+    print(map_dim)
+    starting_map <- CancerSimulationR:::GenerateMap(map_dim)
+    print(starting_map)
+    mutation_set <- CancerSimulationR:::EstablishMutations()
+    print(text_values$mutrate)
+    print(text_values$maxdiv)
+    print(text_values$rounds)
+    patient_preaction_set <- CancerSimulationR:::EstablishPatientPreActions()
+    patient_postaction_set <- CancerSimulationR:::EstablishPatientPostActions()
+    mutation_encoding <- CancerSimulationR:::MakeMutationEncoding(mutation_set)
+    cell_mut_rate <- CancerSimulationR:::GenerateMutationMatrix(map_dim, starting_map, text_values$mutrate)
+    cell_divisions <- CancerSimulationR:::GenerateDivisionMatrix(map_dim, starting_map)
+    cell_states <- CancerSimulationR:::MakeStartingCellStateMatrix(map_dim)
+  })
+  output$text_vals <- renderUI({
+    str1 <- paste("Mutation rate assigned to", text_values$mutrate)
+    str2 <- paste("Max number of cell divisions to", text_values$maxdiv)
+    str3 <- paste("Number of rounds assigned to", text_values$rounds)
+    HTML(paste(str1, str2, str3, sep = "<br/>"))
+  })
 
-  PLOT1 <- eventReactive(input$button1, {
-    # fill in plot
+  ### SIMULATE SINGLE PATIENT
+  observeEvent(input$simulate1, {
+    save_output <- CancerSimulationR:::RunPatientSimulation(text_values$rounds, starting_map, mutation_set,
+                                                                    patient_preaction_set, patient_postaction_set,
+                                                                    mutation_encoding, cell_mut_rate,
+                                                                    cell_divisions, cell_states, text_values$maxdiv)
+  })
+
+  PLOT1 <- eventReactive(input$showmap1, {
+    print(save_output[[1]])
   })
   output$PLOT1 <- renderPlot({
     PLOT1()
   })
+
 
   PLOT2 <- eventReactive(input$button2, {
     # fill in plot
@@ -20,5 +54,4 @@ server <- function(input, output) {
   output$PLOT2 <- renderPlot({
     PLOT2()
   })
-  
 }
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,23 +1,32 @@
 
 ui <- fluidPage(
   navbarPage("Cancer Simulation",
-             tabPanel("PANEL 1",
+             tabPanel("Simulation Set-up",
+                      sidebarLayout(
+                        sidebarPanel(htmlOutput("text_vals")),
+                        mainPanel(h4("STUFF"),
+                                  sliderInput(inputId = "mutationrate", label = "Mutation Rate",
+                                              min = 0.005, max = 0.5, value = 0.05),
+                                  numericInput(inputId = "maxdivisions", label = "Max Allowed Divisions",
+                                               min = 1, max = 10, value = 5, step = 1),
+                                  numericInput(inputId = "numrounds", label = "Number of Rounds",
+                                               min = 5, max = 100, value = 10, step = 5),
+                                  actionButton(inputId = "button1", label = "Submit Values")
+                        ))),
+             tabPanel("Simulate Single Patient",
                       sidebarLayout(
                         sidebarPanel(h4("STUFF"),
-                                     sliderInput(inputId = "mutationrate", label = "Mutation Rate",
-                                                 min = 0.001, max = 0.5, value = 0.05),
-                                     numericInput(inputId = "maxdivisions", label = "Max Allowed Divisions",
-                                                 min = 1, max = 10, value = 5, step = 1),
-                                     numericInput(inputId = "numrounds", label = "Number of Rounds",
-                                                  min = 5, max = 100, value = 10, step = 1),
-                                     actionButton(inputId = "button1", label = "Push Button")
-                        ),
-                        mainPanel(plotOutput("PLOT1")))),
-             tabPanel("PANEL 2",
-                      sidebarLayout(
-                        sidebarPanel(h4("STUFF"),
+                                     actionButton(inputId = "simulate1", label = "Run Simulation"),
+                                     actionButton(inputId = "showmap1", label = "Show Map"),
                                      actionButton(inputId = "button2", label = "Push Button")),
                         mainPanel(plotOutput("PLOT2"))
+                      )
+             ),
+             tabPanel("Simulate Multiple Patient",
+                      sidebarLayout(
+                        sidebarPanel(h4("STUFF"),
+                                     actionButton(inputId = "button3", label = "Push Button")),
+                        mainPanel(plotOutput("PLOT3"))
                       )
              )
   )
