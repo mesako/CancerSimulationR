b393d0e671a57a5d01b63a15fd30f063759323bc
mesako <mesako@stanford.edu>
2020-01-02 09:46:43 -0800

    update variable names

--- inst/app/server.R
+++ inst/app/server.R
@@ -5,38 +5,38 @@ library(reshape)
 
 server <- function(input, output) {
   ### SIMULATION SET-UP
-  text_values <- reactiveValues()
-  observeEvent(input$button1, {
-    text_values$mutrate <- input$mutationrate
-    text_values$maxdiv <- input$maxdivisions
-    text_values$rounds <- input$numrounds
+  single_text_values <- reactiveValues()
+  observeEvent(input$single_values, {
+    single_text_values$mutrate <- input$single_mutationrate
+    single_text_values$maxdiv <- input$single_maxdivisions
+    single_text_values$rounds <- input$single_numrounds
     map_dim <<- c(10, 10)
     starting_map <<- CancerSimulationR:::GenerateMap(map_dim)
     mutation_set <<- CancerSimulationR:::EstablishMutations()
     patient_preaction_set <<- CancerSimulationR:::EstablishPatientPreActions()
     patient_postaction_set <<- CancerSimulationR:::EstablishPatientPostActions()
     mutation_encoding <<- CancerSimulationR:::MakeMutationEncoding(mutation_set)
-    cell_mut_rate <<- CancerSimulationR:::GenerateMutationMatrix(map_dim, starting_map, text_values$mutrate)
+    cell_mut_rate <<- CancerSimulationR:::GenerateMutationMatrix(map_dim, starting_map, single_text_values$mutrate)
     cell_divisions <<- CancerSimulationR:::GenerateDivisionMatrix(map_dim, starting_map)
     cell_states <<- CancerSimulationR:::MakeStartingCellStateMatrix(map_dim)
   })
-  output$text_vals <- renderUI({
+  output$show_single_text <- renderUI({
     str0 <- "<br/>"
-    str1 <- paste("Mutation rate assigned to", text_values$mutrate)
-    str2 <- paste("Max number of cell divisions to", text_values$maxdiv)
-    str3 <- paste("Number of rounds assigned to", text_values$rounds)
+    str1 <- paste("Mutation rate assigned to", single_text_values$mutrate)
+    str2 <- paste("Max number of cell divisions to", single_text_values$maxdiv)
+    str3 <- paste("Number of rounds assigned to", single_text_values$rounds)
     str4 <- "<br/>"
     HTML(paste(str0, str1, str2, str3, str4, sep = "<br/>"))
   })
 
   ### SIMULATE SINGLE PATIENT
-  observeEvent(input$simulate1, {
-    save_output <<- CancerSimulationR:::RunPatientSimulation(text_values$rounds, starting_map, mutation_set,
+  observeEvent(input$single_simulate, {
+    save_output <<- CancerSimulationR:::RunPatientSimulation(single_text_values$rounds, starting_map, mutation_set,
                                                              patient_preaction_set, patient_postaction_set,
                                                              mutation_encoding, cell_mut_rate, map_dim,
-                                                             cell_divisions, cell_states, text_values$maxdiv)
+                                                             cell_divisions, cell_states, single_text_values$maxdiv)
   })
-  final_map <- eventReactive(input$simulate1, {
+  final_map <- eventReactive(input$single_simulate, {
     print(save_output[[1]])
   })
   output$final_map <- renderPlot({
@@ -45,26 +45,26 @@ server <- function(input, output) {
 
 
   ### SIMULATE MULTIPLE PATIENTS
-  text_values2 <- reactiveValues()
-  observeEvent(input$button2, {
-    text_values2$mutrate <- input$mutationrate2
-    text_values2$maxdiv <- input$maxdivisions2
-    text_values2$rounds <- input$numrounds2
+  multi_text_values <- reactiveValues()
+  observeEvent(input$multi_values, {
+    multi_text_values$mutrate <- input$multi_mutationrate
+    multi_text_values$maxdiv <- input$multi_maxdivisions
+    multi_text_values$rounds <- input$multi_numrounds
     map_dim <<- c(10, 10)
     starting_map <<- CancerSimulationR:::GenerateMap(map_dim)
     mutation_set <<- CancerSimulationR:::EstablishMutations()
     patient_preaction_set <<- CancerSimulationR:::EstablishPatientPreActions()
     patient_postaction_set <<- CancerSimulationR:::EstablishPatientPostActions()
     mutation_encoding <<- CancerSimulationR:::MakeMutationEncoding(mutation_set)
-    cell_mut_rate <<- CancerSimulationR:::GenerateMutationMatrix(map_dim, starting_map, text_values$mutrate)
+    cell_mut_rate <<- CancerSimulationR:::GenerateMutationMatrix(map_dim, starting_map, multi_text_values$mutrate)
     cell_divisions <<- CancerSimulationR:::GenerateDivisionMatrix(map_dim, starting_map)
     cell_states <<- CancerSimulationR:::MakeStartingCellStateMatrix(map_dim)
   })
-  output$text_vals2 <- renderUI({
+  output$show_multi_text <- renderUI({
     str0 <- "<br/>"
-    str1 <- paste("Mutation rate assigned to", text_values2$mutrate)
-    str2 <- paste("Max number of cell divisions to", text_values2$maxdiv)
-    str3 <- paste("Number of rounds assigned to", text_values2$rounds)
+    str1 <- paste("Mutation rate assigned to", multi_text_values$mutrate)
+    str2 <- paste("Max number of cell divisions to", multi_text_values$maxdiv)
+    str3 <- paste("Number of rounds assigned to", multi_text_values$rounds)
     str4 <- "<br/>"
     HTML(paste(str0, str1, str2, str3, str4, sep = "<br/>"))
   })
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -4,30 +4,30 @@ ui <- fluidPage(
              tabPanel("Simulate Single Patient",
                       sidebarLayout(
                         sidebarPanel(h4("Set-up Single Simulation"),
-                                     sliderInput(inputId = "mutationrate", label = "Mutation Rate",
+                                     sliderInput(inputId = "single_mutationrate", label = "Mutation Rate",
                                                  min = 0.005, max = 0.5, value = 0.05),
-                                     numericInput(inputId = "maxdivisions", label = "Max Allowed Divisions",
+                                     numericInput(inputId = "single_maxdivisions", label = "Max Allowed Divisions",
                                                   min = 1, max = 10, value = 5, step = 1),
-                                     numericInput(inputId = "numrounds", label = "Number of Rounds",
+                                     numericInput(inputId = "single_numrounds", label = "Number of Rounds",
                                                   min = 5, max = 100, value = 10, step = 5),
-                                     actionButton(inputId = "button1", label = "Submit Values"),
-                                     htmlOutput("text_vals"),
-                                     actionButton(inputId = "simulate1", label = "Run Simulation")),
+                                     actionButton(inputId = "single_values", label = "Submit Values"),
+                                     htmlOutput("show_single_text"),
+                                     actionButton(inputId = "single_simulate", label = "Run Simulation")),
                         mainPanel(plotOutput("final_map"))
                       )
              ),
              tabPanel("Simulate Multiple Patient",
                       sidebarLayout(
                         sidebarPanel(h4("Set-up Multiple Simulations"),
-                                     sliderInput(inputId = "mutationrate2", label = "Mutation Rate",
+                                     sliderInput(inputId = "multi_mutationrate", label = "Mutation Rate",
                                                  min = 0.005, max = 0.5, value = 0.05),
-                                     numericInput(inputId = "maxdivisions2", label = "Max Allowed Divisions",
+                                     numericInput(inputId = "multi_maxdivisions", label = "Max Allowed Divisions",
                                                   min = 1, max = 10, value = 5, step = 1),
-                                     numericInput(inputId = "numrounds2", label = "Number of Rounds",
+                                     numericInput(inputId = "multi_numrounds", label = "Number of Rounds",
                                                   min = 5, max = 100, value = 10, step = 5),
-                                     actionButton(inputId = "button2", label = "Submit Values"),
-                                     htmlOutput("text_vals2"),
-                                     actionButton(inputId = "simulate2", label = "Run Simulations")),
+                                     actionButton(inputId = "multi_values", label = "Submit Values"),
+                                     htmlOutput("show_multi_text"),
+                                     actionButton(inputId = "multi_simulate", label = "Run Simulations")),
                         mainPanel(plotOutput("summary_plots"))
                       )
              )
